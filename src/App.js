2da549072bce67c209c7438688338fe7a7b38a8e
DIBYO-DEY <63423632+DIBO237@users.noreply.github.com>
2022-03-22 18:35:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <BrowserRouter>
       <Navbars />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/"  element={<Home />} />
         <Route path="/details/:id" element={<Details />} />
 
       </Routes>
